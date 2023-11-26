a8276d2ffe0678c9ddb8283cc9d2ea121bba1683
Matheus Estevao <matheus@skinnysoft.com.br>
2023-11-26 19:38:41 -0300

    - Upgrade Laravel to version 10.33 - Disabled routes Register and Login - Changing the welcome page, laravel default

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Auth::routes();
+
+Route::redirect('register', '/');
+Route::redirect('login', '/');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
