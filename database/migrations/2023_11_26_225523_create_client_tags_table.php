af295396d4a8f1117a01f50262f0f29785fb658c
Matheus Estevao <matheus@skinnysoft.com.br>
2023-11-26 20:27:00 -0300

    - Change uuid table users - Create Models and Migrations Tags, Clients and ClientTags

---
+++ database/migrations/2023_11_26_225523_create_client_tags_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('client_tags', function (Blueprint $table) {
+            $table->uuid('id')->primary('id');
+            $table->uuid('client_id')->index()->after('id');
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->uuid('tag_id')->index()->after('id');
+            $table->foreign('tag_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('client_tags');
+    }
+};
